c4a4675acdc503002b37bec1bc583c895c80b9e9
Hiram Arellano <victorhiram.arellano.contractor@bbva.com>
2020-10-12 14:09:12 -0500

    servicio items: agregando configuración para el servidor de configuración

--- springboot-servicio-items/src/main/java/com/formacionbdi/app/items/controller/ItemsController.java
+++ springboot-servicio-items/src/main/java/com/formacionbdi/app/items/controller/ItemsController.java
@@ -1,10 +1,18 @@
 package com.formacionbdi.app.items.controller;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +22,21 @@ import com.formacionbdi.app.items.models.Producto;
 import com.formacionbdi.app.items.models.service.IServiceItem;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
+@RefreshScope
 @RestController
 public class ItemsController {
+	
+	private static Logger log = org.slf4j.LoggerFactory.getLogger(ItemsController.class);
+	
+	@Autowired
+	private Environment env;
 	
 	@Autowired
 	@Qualifier("serviceFeign")
 	private IServiceItem serviceItem;
+	
+	@Value("${configuracion.texto}")
+	private String texto;
 	
 	@GetMapping("/listar")
 	public List<Item> listar(){
@@ -42,5 +59,21 @@ public class ItemsController {
 		
 		return item;
 		
+	}
+	
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto){
+		
+		log.info(texto);
+		Map<String, String> json = new HashMap<>();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+		
+		if(env.getActiveProfiles().length>0 && env.getActiveProfiles()[0].equals("dev")) {
+			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", env.getProperty("configuracion.autor.email"));
+		}
+		
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
 	}
 }
